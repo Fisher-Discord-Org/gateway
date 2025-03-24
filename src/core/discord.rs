d2f80bda121cc0396a623828e6f21912ba6101af
ChaserZ98 <feiyuzheng98@gmail.com>
2025-03-24 06:01:27 -0400

    Update

--- src/core/discord.rs
+++ src/core/discord.rs
@@ -3,13 +3,16 @@ use std::env;
 use serenity::all::GuildId;
 use serenity::client::{Context, EventHandler};
 use serenity::model::gateway::Ready;
+use tracing::instrument;
 
+#[derive(Debug)]
 pub struct Handler;
 
 #[serenity::async_trait]
 impl EventHandler for Handler {
+    #[instrument(skip(self, _ctx, ready))]
     async fn ready(&self, _ctx: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+        tracing::info!("{} is connected!", ready.user.name);
 
         let guild_id = GuildId::new(
             env::var("DEV_GUILD_ID")
